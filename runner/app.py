f69858736b9c4bb23c5b69ed41d0db07ce0c8486
Michael Lindsay <michaelnlindsay@gmail.com>
2025-10-04 16:09:59 -0700

    Conda + Ctools Install

---
+++ runner/app.py
@@ -0,0 +1,9 @@
+import os
+import gammalib
+models=gammalib.GModels()
+print(models)
+
+gammaLibLoc=os.environ['GAMMALIB']
+
+print("Hello, from GFG!")
+print(gammaLibLoc)
---
+++ runner/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+conda config --append channels conda-forge
+conda config --append channels cta-observatory
+
+touch poo.txt
