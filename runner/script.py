91aa80eb704dfa8bc536d76eb3210f31fc42fa64
Michael Lindsay <michaelnlindsay@gmail.com>
2025-10-09 11:31:06 -0700

    light curve sample git ignore for log & fits

--- runner/script.py
+++ runner/script.py
@@ -1,12 +1,16 @@
 import gammalib
 import ctools
+import cscripts
 
 print("CTA Data Pipeline at your service...")
-
-models=gammalib.GModels()
-print(models)
 
 sim = ctools.ctobssim()
 sim["ra"]  = 83.63
 sim["dec"] = 22.01
 sim.execute()
+
+lightcrv = cscripts.cslightcrv()
+lightcrv.execute()
+
+models=gammalib.GModels()
+print(models)
